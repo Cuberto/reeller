a79d9ccf378a16551c15a3759a645224998d01cd
Artem Draft <artemon_93@mail.ru>
2022-08-16 23:49:51 +0300

    Fix Reeller separate export

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import Filler from './module/Filler';
 import Reeller from './module/Reeller';
 import ScrollerPlugin from './plugin/ScrollerPlugin';
 
-export {Reeller as default, Filler, ScrollerPlugin};
+export {Reeller as default, Reeller, Filler, ScrollerPlugin};
