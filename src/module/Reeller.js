cd342d03f63f170ab86a316f167d0647e69fd267
Artem Draft <artemon_93@mail.ru>
2023-10-09 20:20:01 +0300

    v0.0.5

--- src/module/Reeller.js
+++ src/module/Reeller.js
@@ -3,7 +3,7 @@
  * https://github.com/Cuberto/reeller
  * https://cuberto.com/
  *
- * @version 0.0.4
+ * @version 0.0.5
  * @license The MIT License (MIT)
  * @author Cuberto, Artem Dordzhiev (Draft)
  */
